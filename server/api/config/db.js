541a78c3af6c89198a60fedd31a0fde28e4251bd
tituskipkirui087 <tituskipkirui008@gmail.com>
2026-06-30 03:24:47 +0300

    Fix prisma client in api directory

--- server/api/config/db.js
+++ server/api/config/db.js
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
-const isProd = process.env.NODE_ENV === 'production' || process.env.VERCEL;
-export const prisma = new PrismaClient({
+
+const prisma = new PrismaClient({
     log: ['error'],
 });
+
+export { prisma };
